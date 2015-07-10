d6ea93c9c9b1d5ca9f9a1e13b4d834541619a1c0
Duncan Marsh <dwolfm@gmail.com>
2015-07-09 17:20:14 -0700

    added ex3 printf exercise

---
+++ ex3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char **argv){
+  char *name = "Duncan Marsh";
+  int age = 24;
+  float shred2ZoneRatio = 0.45;
+
+  printf("%s is my name.\n", name);
+  printf("I am %d years old.\n", age);
+  printf("my shred 2 zone ration is only %f./n", shred2ZoneRatio);
+
+  return 0;
+}
