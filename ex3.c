3546f5a6d881b50826bafaa8b71587b8c53e9d53
Duncan Marsh <dwolfm@gmail.com>
2015-07-09 17:43:25 -0700

    fixed the bugs in ex4

--- ex3.c
+++ ex3.c
@@ -25,9 +25,9 @@
 int main(int argc, char **argv){
   char *name = "Duncan Marsh";
   int age = 24;
-  float shred2ZoneRatio ;
+  float shred2ZoneRatio = 33.33;
 
-  printf("%s is my name.\n");
+  printf("%s is my name.\n", name);
   printf("I am %d years old.\n", age);
   printf("my shred 2 zone ration is only %f./n", shred2ZoneRatio);
 
